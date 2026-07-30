fa2d58346e2d8bc36f068e4f707130ad1e8dfcd4
Srija105 <srijachedella207@gmail.com>
2026-07-30 20:36:38 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,17 @@
+pi=22/7
+print(type(pi))
+
+
+#for = 4
+#print(for)
+
+
+P = float(input("Enter Principal amount: "))
+R = float(input("Enter Rate of interest: "))
+T = float(input("Enter Time in years: "))
+
+# Calculating Simple Interest
+SI = (P * R * T) / 100
+
+# Display result
+print("Simple Interest =", SI)
