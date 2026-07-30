f766c470b6d93a5246b22bd48e779b23a8f693ad
Srija105 <srijachedella207@gmail.com>
2026-07-30 20:36:13 +0530

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,16 @@
+result = format(145, 'o')
+print(result)
+radius = 94
+pi = 3.14
+
+area = pi * radius * radius
+water = area * 1.4
+
+print("Area of Pond =", int(area))
+print("Total Water =", int(water), "liters")
+distance = 490      # meters
+time = 5 * 60       # seconds
+
+speed = distance / time
+
+print("Speed =", int(speed), "m/s")
